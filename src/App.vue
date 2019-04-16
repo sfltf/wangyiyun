69081203954a4ad9a3ec7466bb443d65eb508038
Tengfei Lian <2837526923@qq.com>
2019-04-16 17:47:23 +0800

    去除图片

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -12,12 +11,23 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0 auto;
+}
+a {
+  text-decoration: none;
+}
+ul,li {
+  list-style: none;
+  margin-block-start: 0;
+  margin-block-end: 0;
+  padding-inline-start: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
