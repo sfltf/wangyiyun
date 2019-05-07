10488d282d3eea95237a4e3e25b0435faabddc3f
liantengfei <2837526923@qq.com>
2019-05-07 17:44:46 +0800

    添加播放器组件

---
+++ src/components/playSong/playList/playList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="play-list-bg">
+    <div class="play-top">
+      <span class="play-list-totalN">
+        播放列表({{songList.length}})
+      </span>
+      <a href="javascript:;" class="collection-all">
+        <span class="ico-add playList-bg"></span>
+        收藏全部</a>
+    </div>
+    <div class="play-content">
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    playlist: Array
+  },
+  data() {
+    return {
+      songList: this.playlist
+    }
+  },
+  methods: {
+    getPlayList() {
+      console.log(this.songList);
+    }
+  },
+  mounted() {
+    this.getPlayList();
+  }
+}
+
+</script>
+<style src="./playList.css"></style>
--- src/components/playSong/playSong.vue
+++ src/components/playSong/playSong.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="bottom-play-bg">
-    <div style="width: 731px;margin: 0 auto;">
+    <div style="position:relative;width: 731px;margin: 0 auto;">
       <div class="play-button">
         <a class="play-pre bottom-play-bg" href="javascript:;" title="上一首(ctrl+←)"></a>
-        <a class="play-current bottom-play-bg" href="javascript:;" title="播放/暂停(p)" ref="operatSong" @click="playSong"></a>
+        <a :class="{'play-current-pause': isNotPause}" class="play-current bottom-play-bg" href="javascript:;" title="播放/暂停(p)" ref="operatSong" @click="playSong"></a>
         <a class="play-next bottom-play-bg" href="javascript:;" title="下一首(ctrl+→)"></a>
       </div>
       <div class="bottom-play-flag">
-        <img class="play-flag-img" :src="songDetail.al.picUrl" alt="专辑图片">
+        <img class="play-flag-img" :src="picUrl" alt="专辑图片">
       </div>
       <div class="bottom-play">
         <span class="author-information">
           <a class="song-name" href="">{{songDetail.name}}</a>
           <a class="author-name" href="">{{songDetail.ar[0].name}}</a>
         </span>
-        <el-slider v-model="playProgress" :show-tooltip="false" :min='minNumber' :max="totalTime" @change="changeProgress" style="position:relative;bottom: -35px;"></el-slider>
+        <el-slider v-model="playProgress" :show-tooltip="false" :min='0' :max="totalTime" @change="changeProgress" style="position:relative;bottom: -35px;"></el-slider>
         <span class="song-time">
           <em>{{currentTime}} </em>
           / {{songDetail.songtime}}
@@ -24,29 +24,59 @@
         <a href="javascript:;" class="play-add bottom-play-bg"></a>
         <a href="javascript:;" class="play-share bottom-play-bg"></a>
       </span>
+      <div class="right-flag bottom-play-bg">
+        <div :class="{volume: isHiddenVolume}" class="block volume-bg">
+          <el-slider v-model="volume" vertical height="101px" @change="operatVolume" :show-tooltip="false" style="bottom: -12px;;width: 4px;">
+          </el-slider>
+        </div>
+        <a href="javascript:;" class="icn-vol bottom-play-bg" :class="{'icn-volno' :isMute}" title="音量" @click="showHiddenVolume" ref="iconVolume" id="volume"></a>
+        <el-tooltip class="item" effect="dark" :content="title" :visible-arrow="false" placement="top">
+          <a href="javascript:;" :class="{'icn-loop':cycle,'icn-one':singleCycle,'icn-shuffle':random}" class="bottom-play-bg" @click="switchPlayMode" :title="title"></a>
+        </el-tooltip>
+        <el-tooltip class="item" effect="dark" :manual="true" content="已开始播放" placement="top">
+          <a href="javascript:;" class="play-list bottom-play-bg" style="text-decoration: none;" @click="isShowHiddenPlayList">{{playList.length}}</a>
+        </el-tooltip>
+      </div>
     </div>
+    <play-list :class="{'play-list-hidden':isHiddenPlayList}" :playlist="playList" ></play-list>
     <audio :src="playSongUrl" autoplay id="player" ref="player"></audio>
   </div>
 </template>
 <script>
+import playList from './playList/playList.vue'
 export default {
+  components: {
+  	'play-list': playList
+  },
   data() {
     return {
-      playSongUrl: '',
-      status: false,
-      playProgress: 0,
-      songDetail: {},
-      currentTime: '00:00',
-      totalTime: 0,
-      minNumber: 0
+      playSongUrl: '', //歌曲url
+      playProgress: 0, //播放进度
+      songDetail: {}, //歌曲详情
+      picUrl: '',
+      currentTime: '00:00', //当前歌曲时长
+      totalTime: 0, //歌曲时长，进度条最大值
+      volume: 50, //音量
+      isMute: true, //是否静音
+      isHiddenVolume: true, //音量调整隐藏
+      isHiddenPlayList: true, //当前播放列表隐藏
+      switchNum: 1, //播放方式切换次数
+      cycle: true, //循环
+      singleCycle: false, //单曲循环
+      random: false, //随机,
+      title: '循环',
+      playListN: 0, //播放列表数量
+      playList: [], //播放列表
+      isNotPause: false //是否暂停
     }
   },
   methods: {
+    // 歌曲播放
     playSong(e) {
       let _self = this;
       let audio = this.$refs.player;
       if (audio.paused && this.playSongUrl !== '') {
-        e.target.setAttribute('class', 'play-current play-current-pause bottom-play-bg');
+        this.isNotPause = true;
         audio.play();
         let processLine = setInterval(function() {
           _self.currentTime = Math.floor(audio.currentTime);
@@ -73,12 +103,12 @@ export default {
           if (audio.ended) {
             _self.playProgress = 0;
             _self.currentTime = '00:00';
-            _self.$refs.operatSong.setAttribute('class', 'play-current bottom-play-bg')
+            _self.isNotPause = false;
             clearInterval(processLine);
           }
         }, 1000)
       } else {
-        e.target.setAttribute('class', 'play-current bottom-play-bg');
+        this.isNotPause = false;
         audio.pause();
       }
     },
@@ -87,7 +117,7 @@ export default {
     },
     // 改变进度条进度
     changeProgress(val) {
-    	let _self = this;
+      let _self = this;
       let audio = this.$refs.player;
       this.playProgress = val;
       if (this.playProgress < 10) {
@@ -108,9 +138,58 @@ export default {
       }
       audio.currentTime = val;
       if (audio.paused && this.playSongUrl !== '') {
-      	audio.play();
-      	_self.$refs.operatSong.setAttribute('class', 'play-current play-current-pause bottom-play-bg');
+        audio.play();
+        this.isNotPause = true;
       }
+    },
+    // 改变音量显示隐藏
+    showHiddenVolume() {
+      this.isHiddenVolume = !this.isHiddenVolume;
+    },
+    // 改变音量
+    operatVolume(val) {
+      let audio = this.$refs.player;
+      console.log(val);
+      if (parseInt(val) === 0) {
+        this.isMute = true;
+        audio.volume = 0;
+      } else {
+        this.isMute = false;
+        audio.volume = val / 100;
+      }
+    },
+    //初始化检测音量确定音量图标
+    checkVolume() {
+      if (this.volume === 0) {
+        this.isMute = true;
+      } else {
+        this.isMute = false;
+      }
+    },
+    // 切换播放方式
+    switchPlayMode() {
+      ++this.switchNum;
+      console.log(this.switchNum);
+      if (this.switchNum % 3 === 0) {
+        this.singleCycle = true;
+        this.cycle = false;
+        this.random = false;
+        this.title = "单曲循环";
+      } else if (this.switchNum % 2 === 0) {
+        this.singleCycle = false;
+        this.cycle = false;
+        this.random = true;
+        this.title = "随机";
+      } else if (this.switchNum % 1 === 0) {
+        this.singleCycle = false;
+        this.cycle = true;
+        this.random = false;
+        this.title = "循环"
+      }
+    },
+    // 播放列表隐藏显示
+    isShowHiddenPlayList() {
+    	this.isHiddenPlayList = !this.isHiddenPlayList;
     }
   },
   created() {
@@ -123,6 +202,7 @@ export default {
     }).then(function(res) {
       console.log('获取歌曲内容')
       _self.songDetail = res.data.songs[0];
+      _self.picUrl = _self.songDetail.al.picUrl;
       let songTime = _self.songDetail.dt / (1000 * 60);
       let minute = parseInt(songTime);
       let second = ((songTime - minute) * 60).toFixed(0);
@@ -149,21 +229,22 @@ export default {
         url: id
       }).then(function(res) {
         console.log('获取歌曲')
-        console.log(res.data.data[0])
         _self.playSongUrl = res.data.data[0].url;
-        _self.$refs.operatSong.setAttribute('class', 'play-current play-current-pause bottom-play-bg');
+        _self.isNotPause = true;
       }).catch(function(e) {
         console.log(e)
       });
 
-      // 音乐相关信息
+      // 音乐相关信息展示
       this.$http({
         method: 'post',
         url: detailId
       }).then(function(res) {
         console.log('获取歌曲内容')
         let audio = _self.$refs.player;
+        console.log(res.data)
         _self.songDetail = res.data.songs[0];
+        _self.picUrl = _self.songDetail.al.picUrl;
         _self.totalTime = Math.floor(_self.songDetail.dt / 1000);
         let songTime = _self.songDetail.dt / (1000 * 60);
         let minute = Math.floor(songTime);
@@ -197,18 +278,37 @@ export default {
           }
           // 进度条
           _self.playProgress = audio.currentTime;
+          // 初始音量
+          audio.volume = _self.volume / 100;
           // 播放器状态回归
           if (audio.ended) {
             _self.playProgress = 0;
             _self.currentTime = '00:00';
-            _self.$refs.operatSong.setAttribute('class', 'play-current bottom-play-bg')
+            _self.isNotPause = false;
             clearInterval(processLine);
           }
         }, 1000)
+
+        // 添加播放列表
+        try {
+          if (_self.playList.length === 0) {
+            _self.playList.push(_self.songDetail);
+          } else {
+            _self.playList.forEach(function(item, index) {
+              if (val != item.id && index == _self.playList.length - 1) {
+                _self.playList.push(_self.songDetail);
+              }
+            })
+          }
+        } catch (e) {
+          console.log(e)
+        }
       }).catch(function(e) {
         console.log(e)
       });
     })
+    //检测音量
+    this.checkVolume()
   }
 }
 
